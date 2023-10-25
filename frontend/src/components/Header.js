49a555990016406236ca579355b47e77ff6cf681
Ekta Choudhary <ektachoudhary309@gmail.com>
2023-10-25 14:36:56 +0530

    Added frontend UI

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+
+      <ul>
+        <li>
+          <Link to="/login">
+            <FaSignInAlt />
+            Login
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/register">
+            <FaSignOutAlt />
+            Logout
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/">
+            <FaUser />
+            User
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
